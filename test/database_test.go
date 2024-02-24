7aa953e539a70b4a473ceeda10bb873233241610
VanillaSkys <filllovefamily@gmail.com>
2024-02-25 04:41:53 +0700

    Update database_test.go

--- test/database_test.go
+++ test/database_test.go
@@ -1,41 +1,41 @@
 package test
 
 import (
-	"context"
+	"database/sql"
 	"os"
 	"testing"
 
-	"github.com/jackc/pgx/v4/pgxpool"
 	_ "github.com/lib/pq"
 )
 
-// func TestDatabaseConnection(t *testing.T) {
-// 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
-// 	if err != nil {
-// 		t.Fatalf("failed to open database connection: %v", err)
-// 	}
-// 	defer db.Close()
-
-//		// Now you can perform your tests using the database connection
-//	}
-var DBTest *pgxpool.Pool
-
 func TestDatabaseConnection(t *testing.T) {
-
-	config, err := pgxpool.ParseConfig(os.Getenv("DATABASE_URL"))
-	if err != nil {
-		t.Fatalf("failed to config database connection: %v", err)
-	}
-
-	db, err := pgxpool.ConnectConfig(context.Background(), config)
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		t.Fatalf("failed to open database connection: %v", err)
 	}
+	defer db.Close()
 
-	// Check the connection
-	// if err := DB.Ping(context.Background()); err != nil {
-	// 	DB.Close()
-	// 	return nil, err
-	// }
-	DBTest = db
+	// Now you can perform your tests using the database connection
 }
+
+// var DBTest *pgxpool.Pool
+
+// func TestDatabaseConnection(t *testing.T) {
+
+// 	config, err := pgxpool.ParseConfig(os.Getenv("DATABASE_URL"))
+// 	if err != nil {
+// 		t.Fatalf("failed to config database connection: %v", err)
+// 	}
+
+// 	db, err := pgxpool.ConnectConfig(context.Background(), config)
+// 	if err != nil {
+// 		t.Fatalf("failed to open database connection: %v", err)
+// 	}
+
+// 	// Check the connection
+// 	// if err := DB.Ping(context.Background()); err != nil {
+// 	// 	DB.Close()
+// 	// 	return nil, err
+// 	// }
+// 	DBTest = db
+// }
