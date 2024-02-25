78baa0a9d00e093ac50f38c3eab95a4d795e9cfb
VanillaSkys <filllovefamily@gmail.com>
2024-02-25 13:19:59 +0700

    update

--- test/product_test.go
+++ test/product_test.go
@@ -2,74 +2,78 @@ package test
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"os"
 	"testing"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/VanillaSkys/golang/repository"
 	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/joho/godotenv"
 )
 
-func TestSave(t *testing.T) {
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
-	config, err := pgxpool.ParseConfig(os.Getenv("DATABASE_URL"))
-	if err != nil {
-		t.Fatalf("failed to config database connection: %v", err)
-	}
-
-	db, err := pgxpool.ConnectConfig(context.Background(), config)
-	if err != nil {
-		t.Fatalf("failed to open database connection: %v", err)
-	}
-
-	r := repository.New("123", db)
-
-	id := "test"
-	name := "test"
-
-	if err := r.Save(id, name); err != nil {
-		t.Errorf("unexpected error: %v", err)
-	}
-
-}
-
 // func TestSave(t *testing.T) {
-// 	err := godotenv.Load()
+// 	// err := godotenv.Load()
+// 	// if err != nil {
+// 	// 	log.Fatal("Error loading .env file")
+// 	// }
+// 	config, err := pgxpool.ParseConfig(os.Getenv("DATABASE_URL"))
 // 	if err != nil {
-// 		log.Fatal("Error loading .env file")
-// 	}
-// 	fmt.Println(os.Getenv("DATABASE_URL"))
-// 	db, mock, err := sqlmock.New()
-// 	if err != nil {
-// 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-// 	}
-// 	defer db.Close()
-
-// 	pool, err := pgxpool.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-// 	if err != nil {
-// 		t.Fatalf("failed to connect to pgxpool: %v", err)
+// 		t.Fatalf("failed to config database connection: %v", err)
 // 	}
 
-// 	r := repository.New("123", pool)
+// 	db, err := pgxpool.ConnectConfig(context.Background(), config)
+// 	if err != nil {
+// 		t.Fatalf("failed to open database connection: %v", err)
+// 	}
 
-// 	id := "test product"
-// 	name := "test product"
+// 	r := repository.New("123", db)
 
-// 	mock.ExpectExec("INSERT INTO product ").
-// 		WithArgs(name).
-// 		WillReturnResult(sqlmock.NewResult(1, 1)).
-// 		WillReturnError(nil)
+// 	id := "test"
+// 	name := "test"
 
 // 	if err := r.Save(id, name); err != nil {
 // 		t.Errorf("unexpected error: %v", err)
 // 	}
 
-// 	if err := mock.ExpectationsWereMet(); err != nil {
-// 		t.Errorf("there were unfulfilled expectations: %v", err)
-// 	}
 // }
+
+func TestSave(t *testing.T) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	fmt.Println(os.Getenv("DATABASE_URL"))
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	pool, err := pgxpool.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		t.Fatalf("failed to connect to pgxpool: %v", err)
+	}
+
+	r := repository.New("123", pool)
+
+	id := "test product"
+	name := "test product"
+
+	mock.ExpectExec("INSERT INTO product ").
+		WithArgs(id, name).
+		WillReturnResult(sqlmock.NewResult(1, 1)).
+		WillReturnError(nil)
+
+	if err := r.Save(id, name); err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %v", err)
+	}
+}
 
 // func TestSave_Error(t *testing.T) {
 // 	db, mock, err := sqlmock.New()
